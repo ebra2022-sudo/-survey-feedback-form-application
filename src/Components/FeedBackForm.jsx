28c32aaf37dc291ef52e25695c3f0da32241640e
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-04-01 15:33:46 +0300

    add alart and submit even handler

--- src/Components/FeedBackForm.jsx
+++ src/Components/FeedBackForm.jsx
@@ -1,6 +1,44 @@
 import React, { useState } from 'react';
 import './FeedbackForm.css'; 
+import { useEffect } from 'react';
 export const FeedbackForm = () => {
+    const [formdata, setFormData] = useState(
+        {
+            name: '',
+            email: '',
+            feedback: ''
+        }
+    );
+
+    const [togle, settogle] = useState(false);
+
+    useEffect(
+        () => {
+            console.log(formdata.email);
+        },
+        [togle]
+        );
+
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            const confirmationMessage = `
+              Name: ${formdata.name}
+              Email: ${formdata.email}
+              Feedback: ${formdata.feedback}
+            `;
+            const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
+            if (isConfirmed) {
+              console.log('Submitting feedback:', formdata);
+              setFormData({
+                name: '',
+                email: '',
+                feedback: ''
+              });
+              alert('Thank you for your valuable feedback!');
+            }
+          };
+       
 
 
 
@@ -15,18 +53,35 @@ export const FeedbackForm = () => {
           <input
           type="text"
           name="name"
+          value={formdata.name}
+          onChange={input => setFormData({
+            ...formdata,
+            name: input.target.value
+          })}
           placeholder="Your Name"
         />
         <input
           type="email"
           name="email"
+          value={formdata.email}
+          onChange={input => setFormData({
+            ...formdata,
+            email: input.target.value
+          })}
           placeholder="Your Email"
         />
         <textarea
           name="feedback"
           placeholder="Your Feedback"
+          value={formdata.feedback}
+          onChange={input => 
+            setFormData({
+                ...formdata,
+                feedback: input.target.value
+              })
+          }
         ></textarea>
-        <button type="submit">Submit Feedback</button>
+        <button onClick={(event) => {handleSubmit(event)}} type="submit">Submit Feedback</button>
         </form>
       </>
     );
