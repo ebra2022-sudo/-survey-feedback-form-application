2c127b697d156508fb11dbdddbaa6bcdc869402b
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-04-01 14:56:29 +0300

    Add the Introductiion component

---
+++ src/Components/FeedBackForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './FeedbackForm.css'; 
+export const FeedbackForm = () => {
+
+    return (
+      <>
+      <nav>
+      Tell Us What You Think
+      </nav>
+        <form className="feedback-form">
+          <h2>We'd Love to Hear From You!</h2>
+          <p>Please share your feedback with us.</p>
+        </form>
+      </>
+    );
+  };
+  
